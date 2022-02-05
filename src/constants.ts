2401709c42febad795808fed9883429425e981fb
Minimoa <12183385@inha.edu>
2022-02-05 23:21:35 +0900

    .

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
+
+// notion document url
 export const TermsOfService = 'https://bit.ly/3HpPHi8';
 export const PrivacyPolicy = 'https://bit.ly/3gmhMLn';
 export const TermsOfEmail = 'https://bit.ly/3J10lMY';
